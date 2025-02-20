3f4534fb86406a197a6b13684d26aa1347dc096f
RezaNrjaman <rezanrjaman121204@gmail.com>
2025-02-20 16:21:53 +0700

    pertemuan 3

--- P3/Tes1aMain.java
+++ P3/Tes1aMain.java
@@ -4,8 +4,8 @@ public class Tes1aMain {
     public static void main(String[] args) {
         StrukturList list = new StrukturList();
         list.addTail(3);
-        list.addTail(4);
-        list.addTail(5);
+        list.addTail(2);
+        list.addTail(1);
 
         System.out.println("Elemen: ");
         list.displayElement();
